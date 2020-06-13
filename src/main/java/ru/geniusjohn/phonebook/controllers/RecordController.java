602fdf16e0e380eb53ceae86a6cab67000c52d67
Evgeny <John_G@inbox.ru>
2020-06-13 15:38:26 +0300

    refactoring

--- src/main/java/ru/geniusjohn/phonebook/controllers/RecordController.java
+++ src/main/java/ru/geniusjohn/phonebook/controllers/RecordController.java
@@ -16,8 +16,6 @@ import java.util.Map;
 @Controller
 public class RecordController {
 
-    private static final Logger logger = LoggerFactory.getLogger(RecordController.class);
-
     private RecordRepositories recordRepositories;
     private GroupRepository groupRepository;
 
